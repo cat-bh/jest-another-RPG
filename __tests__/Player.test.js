ccfdaad0046c3657a6cf4a68e401fb9ec7436c8a
Catherine <catheneghan@gmail.com>
2021-08-16 15:51:22 -0700

    added prototype functions to the player object to get stats and inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
-
 test('creates player object', () => {
     const player = new Player('Dave');
 
@@ -14,4 +12,23 @@ test('creates player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-})
+});
+
+test('get players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
